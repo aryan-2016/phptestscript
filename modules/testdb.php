cc1f7bd705ff1aee2c61b7fdea9d875a70945d29
aryan-2016 <ajayjain1511@gmail.com>
2016-02-17 12:18:06 +0530

    Update testdb.php

--- modules/testdb.php
+++ modules/testdb.php
@@ -15,11 +15,11 @@ $db = new DbPdoClass();
 
 try 
 {
-	$db->prepareQuery('create table users(id INT NOT NULL, user_name character varying(20) NOT NULL, user_password character varying(20) NOT NULL, CONSTRAINT user_pkey PRIMARY KEY (id))');
+	/*$db->prepareQuery('create table users(id INT NOT NULL, user_name character varying(20) NOT NULL, user_password character varying(20) NOT NULL, CONSTRAINT user_pkey PRIMARY KEY (id))');
 	$db->executeQuery();
-	echo '<br>table created';
+	echo '<br>table created';*/
 	
-	/*$db->prepareQuery('INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)');
+	$db->prepareQuery('INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)');
 	$db->bindQueryValue(':user_name', 'John');
 	$db->bindQueryValue(':user_password', 'Smith');
 	$db->executeQuery();
@@ -28,7 +28,7 @@ try
 	$db->prepareQuery('SELECT user_name, user_password FROM users WHERE user_name = :user_name');
 	$db->bindQueryValue(':user_name', 'John');
 	$row = $db->fetchSingleRow();
-	echo "<br>row=<pre>";print_r($row);echo "</pre>";*/
+	echo "<br>row=<pre>";print_r($row);echo "</pre>";
 } 
 catch(PDOException $e) 
 {
