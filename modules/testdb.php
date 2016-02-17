5d3c7793050aeba6d57bc1975bd5075b88b23add
aryan-2016 <ajayjain1511@gmail.com>
2016-02-17 12:27:02 +0530

    Update testdb.php

--- modules/testdb.php
+++ modules/testdb.php
@@ -15,18 +15,20 @@ $db = new DbPdoClass();
 
 try 
 {
-	/*$db->prepareQuery('create table users(id INT NOT NULL, user_name character varying(20) NOT NULL, user_password character varying(20) NOT NULL, CONSTRAINT user_pkey PRIMARY KEY (id))');
+	$db->prepareQuery('create table users(id SERIAL NOT NULL, user_name character varying(20) NOT NULL, user_password character varying(20) NOT NULL, CONSTRAINT user_pkey PRIMARY KEY (id))');
 	$db->executeQuery();
-	echo '<br>table created';*/
+	echo '<br>table created';
 	
-	$db->prepareQuery('INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)');
+	/*$db->prepareQuery('INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)');
 	$db->bindQueryValue(':user_name', 'John');
-	$db->bindQueryValue(':user_password', 'Smith');
+	$db->bindQueryValue(':user_password', 'Smith');*/
+	$db->prepareQuery('INSERT INTO users (user_name, user_password) VALUES ("John", "Smith")');
 	$db->executeQuery();
 	echo '<br>lastInsertId=' . $db->lastInsertId();
 	
-	$db->prepareQuery('SELECT user_name, user_password FROM users WHERE user_name = :user_name');
-	$db->bindQueryValue(':user_name', 'John');
+	/*$db->prepareQuery('SELECT user_name, user_password FROM users WHERE user_name = :user_name');
+	$db->bindQueryValue(':user_name', 'John');*/
+	$db->prepareQuery('SELECT user_name, user_password FROM users WHERE user_name = "John"');
 	//$row = $db->fetchSingleRow();
 	$rows = $db->fetchResultset();
 	echo "<br>row=<pre>";
