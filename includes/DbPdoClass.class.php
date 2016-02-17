5dee0d43d0be6763aed2c1da2a55297b77af3402
aryan-2016 <ajayjain1511@gmail.com>
2016-02-17 11:52:52 +0530

    Update DbPdoClass.class.php

--- includes/DbPdoClass.class.php
+++ includes/DbPdoClass.class.php
@@ -1,11 +1,11 @@
 <?php
 class DbPdoClass
 {
-	private $dbHost = 'ec2-54-225-215-233.compute-1.amazonaws.com';
-	private $dbUserName = 'nqdnablyxgieuj';
-	private $dbPassword = 'sQ-7YLmKlugulMCY9RLM7Jg6O1';
-	private $dbName = 'da8pn1bn2vbs8m';
-	private $dbPort = '5432';
+	private $dbHost; //= 'ec2-54-225-215-233.compute-1.amazonaws.com';
+	private $dbUserName; //= 'nqdnablyxgieuj';
+	private $dbPassword; //= 'sQ-7YLmKlugulMCY9RLM7Jg6O1';
+	private $dbName; //= 'da8pn1bn2vbs8m';
+	private $dbPort; //= '5432';
 	private $dbConnection;
 	private $dbStatement;
 	
@@ -13,13 +13,15 @@ class DbPdoClass
 	{
 		try
 		{
-			 $options = array(
+			/*$options = array(
 								PDO::ATTR_PERSISTENT    => true,
 								PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION
 							);
 			//$this->dbConnection = new PDO('mysql:host=' . $this->dbHost . ';dbname=' . $this->dbName, $this->dbUserName, $this->dbPassword, $options);
 			$dsn = 'pgsql:host=' . $this->dbHost . ';port=' . $this->dbPort . ';dbname=' . $this->dbName . ';user=' . $this->dbUserName . ';password=' . $this->dbPassword;
-			$this->dbConnection = new PDO($dsn);			
+			$this->dbConnection = new PDO($dsn);*/
+			
+			$this->dbConnection = new PDO('pgsql:dbname=da8pn1bn2vbs8m;host=ec2-54-225-215-233.compute-1.amazonaws.com;port=5432;user=nqdnablyxgieuj;password=sQ-7YLmKlugulMCY9RLM7Jg6O1');
 		}
 		catch(Exception $ex)
 		{
