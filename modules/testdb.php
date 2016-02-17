4ee38c719dd0cdacc995fa72335db116df1fd841
aryan-2016 <ajayjain1511@gmail.com>
2016-02-17 12:44:48 +0530

    Update testdb.php

--- modules/testdb.php
+++ modules/testdb.php
@@ -15,24 +15,24 @@ $db = new DbPdoClass();
 
 try 
 {
-	$db->prepareQuery('DROP TABLE users');
+	/*$db->prepareQuery('DROP TABLE users');
 	$db->executeQuery();
 	echo '<br>table droped';
 	
 	$db->prepareQuery('CREATE TABLE users(id SERIAL NOT NULL, user_name character varying(20) NOT NULL, user_password character varying(20) NOT NULL, CONSTRAINT users_pkey PRIMARY KEY (id))');
 	$db->executeQuery();
-	echo '<br>table created';
+	echo '<br>table created';*/
 	
 	/*$db->prepareQuery('INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)');
 	$db->bindQueryValue(':user_name', 'John');
 	$db->bindQueryValue(':user_password', 'Smith');*/
-	$db->prepareQuery('INSERT INTO users(user_name, user_password) VALUES("John", "Smith")');
+	$db->prepareQuery("INSERT INTO users (user_name, user_password) VALUES ('John', 'Smith')");
 	$db->executeQuery();
 	echo '<br>lastInsertId=' . $db->lastInsertId();
 	
 	/*$db->prepareQuery('SELECT user_name, user_password FROM users WHERE user_name = :user_name');
 	$db->bindQueryValue(':user_name', 'John');*/
-	$db->prepareQuery('SELECT user_name, user_password FROM users WHERE user_name = "John"');
+	$db->prepareQuery("SELECT user_name, user_password FROM users WHERE user_name = 'John'");
 	//$row = $db->fetchSingleRow();
 	$rows = $db->fetchResultset();
 	echo "<br>row=<pre>";
@@ -42,5 +42,5 @@ try
 } 
 catch(PDOException $e) 
 {
-    echo 'error: ' . $e->getMessage();
+    echo '<br>error: <pre>' . $e->getMessage();
 }
