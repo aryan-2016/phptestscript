be1312990780ab47dc785edfa9c8eaf768a30405
aryan-2016 <ajayjain1511@gmail.com>
2016-02-17 16:02:01 +0530

    Update testmap.php

--- modules/testmap.php
+++ modules/testmap.php
@@ -28,13 +28,9 @@ $lat = $row['center_lat'];
 $long = $row['center_long'];
 $zoom = $row['zoom'];	
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
+
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <style type="text/css">
-      html { height: 100% }
-      body { height: 100%; margin: 0; padding: 0 }
       #map-canvas { height: 100% }
     </style>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<?php echo $apikey; ?>&sensor=false"></script>
@@ -64,8 +60,4 @@ $zoom = $row['zoom'];
       }
       google.maps.event.addDomListener(window, 'load', initialize);
     </script>
-  </head>
-  <body>
     <div id="map-canvas"/>
-  </body>
-</html>
