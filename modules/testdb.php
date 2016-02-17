06739fd8aa44e10750b6733528186d5394e38689
aryan-2016 <ajayjain1511@gmail.com>
2016-02-17 12:47:09 +0530

    Update testdb.php

--- modules/testdb.php
+++ modules/testdb.php
@@ -26,18 +26,18 @@ try
 	/*$db->prepareQuery('INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)');
 	$db->bindQueryValue(':user_name', 'John');
 	$db->bindQueryValue(':user_password', 'Smith');*/
-	$db->prepareQuery("INSERT INTO users (user_name, user_password) VALUES ('John', 'Smith')");
+	/*$db->prepareQuery("INSERT INTO users (user_name, user_password) VALUES ('John', 'Smith')");
 	$db->executeQuery();
-	echo '<br>lastInsertId=' . $db->lastInsertId();
+	echo '<br>lastInsertId=' . $db->lastInsertId();*/
 	
-	/*$db->prepareQuery('SELECT user_name, user_password FROM users WHERE user_name = :user_name');
-	$db->bindQueryValue(':user_name', 'John');*/
-	$db->prepareQuery("SELECT user_name, user_password FROM users WHERE user_name = 'John'");
-	//$row = $db->fetchSingleRow();
-	$rows = $db->fetchResultset();
+	$db->prepareQuery('SELECT user_name, user_password FROM users WHERE user_name = :user_name');
+	$db->bindQueryValue(':user_name', 'John');
+	//$db->prepareQuery("SELECT user_name, user_password FROM users WHERE user_name = 'John'");
+	$row = $db->fetchSingleRow();
+	//$rows = $db->fetchResultset();
 	echo "<br>row=<pre>";
-	//print_r($row);
-	var_dump($rows);
+	print_r($row);
+	//var_dump($rows);
 	echo "</pre>";
 } 
 catch(PDOException $e) 
